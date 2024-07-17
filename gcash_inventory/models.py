01fb75ff9aa2c94be2806dca054ac972eb5ecb9c
JjayFabor <faborjaylordvhan@gmail.com>
2024-07-17 19:56:45 +0800

    Update Description data

--- gcash_inventory/models.py
+++ gcash_inventory/models.py
@@ -43,7 +43,8 @@ class Transaction(models.Model):
     investment = models.ForeignKey(Investment, on_delete=models.CASCADE)
     amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     fee = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    description = models.TextField(blank=True, null=True)
+    # description = models.TextField(blank=True, null=True)
+    description = models.CharField(max_length=25)
     date = models.DateTimeField(auto_now_add=True)
     transaction_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
 
